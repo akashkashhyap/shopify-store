ee0d22f53f49aaf25fe01ce4e201d2a519e65568
Akash <akash.kashhyap@gmail.com>
2022-03-08 19:25:22 +0530

    Search provider update 18

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,10 @@
 import * as React from "react"
 import { StoreProvider } from "./src/context/store-context"
-import { SearchProvider } from "./src/context/search-provider"
 import "./src/styles/reset.css"
 import "./src/styles/variables.css"
 import "./src/styles/global.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export const wrapRootElement = ({ element }) => (
-  <>
   <StoreProvider>{element}</StoreProvider>
-  <SearchProvider>
-      <SearchPage {...element} />
-    </SearchProvider>
-  </>
 )
