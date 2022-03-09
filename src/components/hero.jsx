db3ee71a7cf8516c88e02609f7b8ffa5bbdf5da8
Akash <akash.kashhyap@gmail.com>
2022-03-09 16:16:32 +0530

    Home page working style update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,33 +1,21 @@
 import React from "react"
-import styled from "styled-components"
+// import styled from "styled-components"
+import {herobanner} from "../pages/index.module.css"
 
 
-const HeroWrapper = styled.section`
-  background: url("${(props) => props.backgroundImage}");
-  height: calc(100vh - 66px);
-  background-size: cover;
-  background-repeat: no-repeat;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  color: #fff;
-  margin-bottom:30px;
-
-  div {
-    max-width: 800px;
-    margin-left: 5%;
-    padding-top: 20px;
-  }
-`
 
 const Hero = ({ title, backgroundImage }) => {
-  // console.log(title)
+  console.log(backgroundImage)
   return (
-    <HeroWrapper backgroundImage={backgroundImage}>
-      <div className="col-md-12">
-        <h1>1 - {title}</h1>
+      <div className={`col-md-12 ${herobanner}`} style={{background:`url(${backgroundImage})`}}>
+        <h1>{title}</h1>
       </div>
-    </HeroWrapper>
+    
+    // <HeroWrapper backgroundImage={backgroundImage}>
+    //   <div className="col-md-12">
+    //     <h1>{title}</h1>
+    //   </div>
+    // </HeroWrapper>
   )
 }
 
